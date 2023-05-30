b521559c047a72dce88a9c7c940c6c739657eb9d
Mino31415 <135031919+Mino31415@users.noreply.github.com>
2023-05-30 15:14:58 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,36 @@
+import win32gui, win32con, win32ui
+import numpy as np
+import cv2 as cv
+
+def windowCapture():
+    w = 330
+    h = 316
+    
+    hwnd = win32gui.FindWindow(None, 'winnable')
+
+    wDC = win32gui.GetWindowDC(hwnd)
+    dcObj = win32ui.CreateDCFromHandle(wDC)
+    cDC = dcObj.CreateCompatibleDC()
+    dataBitMap = win32ui.CreateBitmap()
+    dataBitMap.CreateCompatibleBitmap(dcObj, w, h)
+    cDC.SelectObject(dataBitMap)
+    cDC.BitBlt((0, 0), (w, h), dcObj, (1570, 740), win32con.SRCCOPY)
+
+    signedIntsArray = dataBitMap.GetBitmapBits(True)
+    img = np.frombuffer(signedIntsArray, dtype='uint8')
+    img.shape = (h,w,4)
+
+
+    dcObj.DeleteDC()
+    cDC.DeleteDC()
+    win32gui.ReleaseDC(hwnd, wDC)
+    win32gui.DeleteObject(dataBitMap.GetHandle())
+    img = img[...,:3]
+    img = np.array(img)
+    return img
+
+
+while True:
+    image = cv.resize(windowCapture(),(330*3,316*3))
+    cv.imshow('Minimap', image)
+    cv.waitKey(1)
